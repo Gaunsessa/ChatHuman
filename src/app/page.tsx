dc4a68dcd4edb327dd18ef2d7a8c12e7a8a0986a
Banny Kuang <z5585688@unsw.edu.au>
2025-06-21 18:44:35 +1000

    new logooooo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,7 @@ export default function Home() {
 return (
   <div className="bg-white max-w-4xl m-2">
     <div className='flex flex-1 justify-center align-center items-center'>
-      <img src="/Assets/ChatHuman.png" alt="" className="size-25 p-5 object-centre"/>
-      <h1 className='text-5xl'>
-        ChatHuman
-      </h1>
+      <img src="/Assets/ChatHumanFull.png" alt="" className="p-5 object-centre"/>
     </div>
     <Chat />
   </div>
