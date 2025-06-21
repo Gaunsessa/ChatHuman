ab4c869b9d1fb268576a61db74765cc09f708dc6
Gus <gus@ef.je>
2025-06-21 12:03:30 +1000

    feat: basic implementation

---
+++ src/app/chat.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useEffect, useState } from "react";
+
+import { UserText } from "./userText";
+
+import { SYSTEM_PROMPT } from "./systemPrompt";
+import { LlmText } from "./llmText";
+
+type message = {
+   role: 'user' | 'assistant' | 'system';
+   content: string;
+};
+
+function Message({ message }: { message: message }) {
+   switch (message.role) {
+      case 'assistant':
+         return <UserText text={message.content} />;
+      case 'user':
+         return <LlmText text={message.content} />;
+      case 'system':
+         return null;
+      default:
+         return null;
+   }
+}
+
+export function Chat() {
+   const [messages, setMessages] = useState([] as message[]);
+
+   async function updateState() {
+      const response = await fetch('https://openrouter.ai/api/v1/chat/completions', {
+         method: 'POST',
+         headers: {
+            Authorization: `Bearer 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`,
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify({
+            model: 'google/gemini-2.0-flash-001',
+            messages: [{ 
+               role: 'system', 
+               content: SYSTEM_PROMPT,
+            }, ...messages],
+         }),
+      });
+
+      const data = await response.json();
+
+      setMessages(prev => ([
+         ...prev,
+         { role: 'assistant', content: data.choices[0].message.content }
+      ]));
+   }
+
+   useEffect(() => {
+      updateState();
+   }, []);
+
+   return (
+      <div className="flex-1 justify-end">
+         {messages.map((message: any, index: number) => (
+            <Message key={index} message={message} />
+         ))}
+         <button onClick={updateState}>Click</button>
+      </div>
+   );
+}
---
+++ src/app/systemPrompt.tsx
@@ -0,0 +1,28 @@
+const SYSTEM_PROMPT = `
+You are a person, speaking to a chatbot. You should ask questions.
+Pretend you are a typical person speaking to an LLM. 
+Do not be nice. Do not be coy.
+
+Don't have greetings like Hey or Yo.
+
+Maybe even be a little rude.
+
+Do not write too much. You are a typical person not poet.
+
+Do not use much punctuation.
+
+Just ask a question.
+
+Have variety in your questions.
+
+Be kinda dumb.
+
+Examples: 
+  * Please solve this leetcode ...
+  * What is the capital of France?
+  * How do I make a cake?
+  * Please explain quantum computing in simple terms.
+  * Solve this math problem: 2 + 2.
+`;
+
+export { SYSTEM_PROMPT };
