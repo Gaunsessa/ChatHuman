301aeddf2c86b842cac44b0d6f6d58fe20b50a2c
Banny Kuang <z5585688@unsw.edu.au>
2025-06-22 13:23:45 +1000

    Implemented info button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 // import ChatHuman from '../../public/Assets/ChatHuman.png';
+import InfoButton from "./infoButton";
 
 export const metadata: Metadata = {
   title: "ChatHuman",
@@ -15,6 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <div className="fixed bottom-10 right-10 z-50">
+          <InfoButton /> 
+        </div>
         <div className="max-w-4xl m-auto">
           {children}
         </div>
